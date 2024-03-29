c1392c2d7c0b9314d3dcb45ade32ed7f0cc556eb
AndersonExner <andersonexner@gmail.com>
2024-03-29 10:56:49 -0300

    insere metodo delete

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -3,16 +3,20 @@ package br.com.api.produtos.controle;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 
-
-
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProdutoControle {
 
     @Autowired
@@ -28,9 +32,19 @@ public class ProdutoControle {
         return ps.listar();
     }
 
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
+
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
         return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable Long codigo){
+        return ps.remover(codigo);
     }
 
 }
--- produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -44,4 +44,14 @@ public class ProdutoServico {
         }
     }
 
+    //Metodo para remover produtos
+    public ResponseEntity<RespostaModelo> remover(Long codigo){
+
+        pr.deleteById(codigo);
+
+        rm.setMensagem("O produto foi removido com sucesso");
+        return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
+
+    }
+
 }
